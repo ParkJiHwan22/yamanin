19bf11ed2f3ab39833adce4481634d1bcc8d35b7
ParkJiHwan <bat522@naver.com>
2024-05-14 14:32:28 +0900

    Add tracking and timestamp features across modules

--- src/main/java/com/yamanin/model/service/QuestionServiceImpl.java
+++ src/main/java/com/yamanin/model/service/QuestionServiceImpl.java
@@ -3,10 +3,12 @@ package com.yamanin.model.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.yamanin.model.dao.QuestionDao;
 import com.yamanin.model.dto.Question;
 
+@Service
 public class QuestionServiceImpl implements QuestionService {
 
 	private final QuestionDao questionDao;
